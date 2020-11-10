819036c45e14c37913b9208e8fa57cad51f35235
Jejoon Ryu <rjejoon@gmail.com>
2020-11-09 21:33:05 -0600

    Restructure unit test scripts

--- test/test_termFilter.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-from phase1.main import termFilter
-
-class TestTermFilter(unittest.TestCase):
-
-    def test_termFilter(self):
-        self.assertAlmostEqual(termFilter('''";:?.,!Hello<p></p>'''), 'Hello')
-        
