b4d0a65d0c9f57d337ca5e27d37dd02159577051
Laptop <junhyeon@ualberta.ca>
2020-11-19 21:03:52 -0700

    .gitignore updated, votePost.py bug fixed

--- mini-project2/phase2/votePost.py
+++ mini-project2/phase2/votePost.py
@@ -1,37 +1,37 @@
-from pymongo import MongoClient
 from datetime import datetime
-from phase2.postQA import genID, validInput
+
 from bcolor import bcolor
+from phase2.getValidInput import getValidInput
+from phase2.postQA import genID
 
 def votePost(votes, uid, targetPid):
-
-    print(bcolor.pink('\n< Vote on a Post >'))
-
-    cursor = votes.find_one({"$and": [{"UserId": uid}, {"PostId": targetPid}]})
-
-    if cursor:
-        print(bcolor.errmsg("error: you've already voted on this post."))
-    else:
-        prompt = '\nConfirmation: Do you want to vote on this post? [y/n] '
-
-        uin = validInput(prompt, ['y', 'n'])
-
-        if uin == 'y':
-
-            vid = genID(votes)
-            crdate = str(datetime.now())
-
-            vote = {
-                "Id": vid,
-                "PostId": targetPid,
-                "VoteTypeId": 2,
-                "CreationDate": crdate
-            }
-
-            if uid:
-                vote['UserId'] = uid
-
-            votes.insert_one(vote)
-            print(bcolor.green('\nVoting Completed!'))
-            
-    print()
+	print(bcolor.pink('\n< Vote on a Post >'))
+	
+	cursor = votes.find_one({"$and": [{"UserId": uid}, {"PostId": targetPid}]})
+	
+	if cursor:
+		print(bcolor.errmsg("error: you've already voted on this post."))
+	else:
+		prompt = '\nConfirmation: Do you want to vote on this post? [y/n] '
+		
+		uin = getValidInput(prompt, ['y', 'n'])
+		
+		if uin == 'y':
+			
+			vid = genID(votes)
+			crdate = str(datetime.now())
+			
+			vote = {
+				"Id"          : vid,
+				"PostId"      : targetPid,
+				"VoteTypeId"  : 2,
+				"CreationDate": crdate
+			}
+			
+			if uid:
+				vote['UserId'] = uid
+			
+			votes.insert_one(vote)
+			print(bcolor.green('\nVoting Completed!'))
+	
+	print()
