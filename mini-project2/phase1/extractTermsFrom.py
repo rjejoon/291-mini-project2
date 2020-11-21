78719fe79585dadaa72cdb5438c395f2498ed017
Jejoon Ryu <rjejoon@gmail.com>
2020-11-20 20:19:37 -0600

    use simple isalnum instead of string.punctutaion and string.whitespaces reduced running time by ~40%

--- mini-project2/phase1/extractTermsFrom.py
+++ mini-project2/phase1/extractTermsFrom.py
@@ -6,7 +6,7 @@ def extractTermsFrom(postDoc: dict) -> list:
     Extracts unique terms from the title and body if those fields exist in the given post document, 
     and returns them in a list.
     '''
-    terms = []
+    terms = set()
     if 'Title' in postDoc:
         filterTerms(terms, postDoc['Title'])
         
@@ -16,10 +16,10 @@ def extractTermsFrom(postDoc: dict) -> list:
     if 'Tags' in postDoc:
         filterTerms(terms, postDoc['Tags'])
 
-    return terms
+    return list(terms)
 
 
-def filterTerms(terms: list, s: str):
+def filterTerms(terms: set, s: str):
     '''
     Filters out alphanumeric terms that are at least 3 chars long from the given string and
     returns only the unique ones.
@@ -27,17 +27,18 @@ def filterTerms(terms: list, s: str):
     if len(s) == 0:
         return 
 
+    # does not handle for case-insensitive
     start = 0       
     i = 0
     for i in range(len(s.strip())):
-        if s[i] in string.whitespace or s[i] in string.punctuation:
+        if not s[i].isalnum():
             if i - start >= 3:    # len of term must be larger than 3 
-                terms.append(s[start:i].lower())
+                terms.add(s[start:i])
             start = i + 1
 
     if s[i].isalnum(): 
         if (i - start) + 1 >= 3:
-            terms.append(s[start:i+1].lower())
+            terms.add(s[start:i+1])
 
 
 
