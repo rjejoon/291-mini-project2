c5fe4b9c20e61cee70d861fbd7a2230e99b43b5f
Jejoon Ryu <rjejoon@gmail.com>
2020-11-21 03:59:50 -0600

    update setup.py b/c it wouldn't build on linux

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from Cython.Build import cythonize
 extensions = [
         Extension('mini_project2.phase1.extractTermsFrom', ['mini_project2/phase1/extractTermsFrom.pyx'],
             language='c++',
-            extra_compile_args=['-std=c++11', '-mmacosx-version-min=10.9', '-D_hypot=hypot', '-stdlib=libc++'])
+            extra_compile_args=['-std=c++11', '-D_hypot=hypot'])
 
 
         ]
