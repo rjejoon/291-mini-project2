1ab0f74ee2232a5bcd9f205a5d6e125502ba7ffd
whqkdhfh13 <junhyeon@ualberta.ca>
2020-11-24 19:31:35 -0700

    working..?

--- test.py
+++ test.py
@@ -6,10 +6,10 @@ client = motor.motor_asyncio.AsyncIOMotorClient(port=port)
 db = client['291db']
 dir_path = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(dir_path, "Votes.json"), 'r') as f:
-    db['votes'].insert_many(json.load(f)["Votes"]['row'], ordered=False)
+    db['votes'].insert_many(json.load(f)["votes"]['row'], ordered=False)
 
 with open(os.path.join(dir_path, "Tags.json"), 'r') as f:
-    db['tags'].insert_many(json.load(f)["Tags"]['row'], ordered=False)
+    db['tags'].insert_many(json.load(f)["tags"]['row'], ordered=False)
 
 
 
